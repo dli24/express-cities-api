a9fa79c5fe85b5cc27d6d0bc154287d32427328f
dli24 <dli24@buffalo.edu>
2019-04-09 10:29:39 -0700

    Adds index and sshow routes for cities

--- server.js
+++ server.js
@@ -9,15 +9,31 @@ app.use((req, res, next) => {
     next();
   });
 
+//----------------------------------TEMP DATA-------------------------------------
+
+const cities = [
+    {name: 'San Francisco', description: 'Foggy city by the bay'},
+    {name: 'Los Angeles', description: 'Fun in the Sun'},
+];
+
 //---------------------------------ROUTES----------------------------------------
 
-
-
-
-
-//--------------------GET ROOT ROUTE
+// GET ROOT ROUTE
 app.get('/', (req,res)=>{
     res.send('<h1>EXPRESS DYNAMIC CITIES</h1>')
+})
+
+// GET API INDEX Cities ROUTE
+app.get('/api/cities', (req,res)=>{
+    // console.log("req receive")
+    res.json(cities);
+
+})
+
+//GET API SHOW CITIES ROUTE
+app.get('/api/cities/:index', (req, res)=>{
+    const city = cities[req.params.index] || `No city exist at index ${req.param.index}`;
+    res.json(city);
 })
 
 
